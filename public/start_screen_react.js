9c1134063636518097478f6dce799e3775e0082d
ryuyryu <yamaryu2007@gmail.com>
2025-06-12 09:14:33 +0900

    スタート画面タイトルのサイズを元に戻す

--- public/start_screen_react.js
+++ public/start_screen_react.js
@@ -32,12 +32,13 @@ function StartScreen() {
         onClick: handleClick,
       },
       React.createElement(
-      'div',
-      {
-        className: 'absolute left-0 w-full text-center font-bold three-d-text painted-text spotlight-text',
-        style: { fontSize: '25vh', top: '-8px' }
-      },
-        'econ'
+        'h1',
+        {
+          className:
+            'font-extrabold text-[#00fb00] animate-pulse drop-shadow-[0_0_5px_#00fb00] absolute left-0 w-full text-center',
+          style: { fontSize: '25vh', top: '-8px' }
+        },
+        'ECON'
       )
     )
   );
